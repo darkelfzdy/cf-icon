9dcd6fdded0f20bbf8a56d0baca5127bd4426ca3
darkelfzdy <darkelfzdy@qq.com>
2025-07-05 10:46:32 +0800

    48*48

--- src/index.ts
+++ src/index.ts
@@ -250,7 +250,7 @@ export default {
         });
       }
 
-      const resizedImageData = await resize(imageData, { width: 32, height: 32 });
+      const resizedImageData = await resize(imageData, { width: 48, height: 48 });
       const finalIconBuffer = await pngEncode(resizedImageData);
       
       return new Response(finalIconBuffer, {
